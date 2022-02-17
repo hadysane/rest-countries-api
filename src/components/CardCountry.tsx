36d052d15a042988f896478feab3c3a884e3d5b4
Hady Sané <hady.sane@baulders.com>
2022-02-17 21:52:44 +0100

    card contries on goning

--- src/components/CardCountry.tsx
+++ src/components/CardCountry.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const CardCountry = () => {
     return (
-        <div>
+        <div className="card">
+
+           
+           
+
+            <div className="card-body">
+                <h2>Germany</h2>
+                <p><span>Population:</span> 81,770,900</p>
+                <p><span>Region:</span>Europe</p>
+                <p><span>Capital:</span>Berlin</p>
+            </div>
             
         </div>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { BiMoon } from 'react-icons/bi'
 
 const Navbar = () => {
     return (
-        <div>
-            <p>Where in the world?</p>
+        <nav className="navbar navbar-light">
+            <div className='container d-flex justify-content-between align-item-center'>
+                <p className='w-800'>Where in the world?</p>
 
-            <button> Dark Mode</button>
-        </div>
+                <button className='btn d-flex align-item-center gap-8'> <BiMoon/>  Dark Mode</button>
+            </div>
+            
+        </nav>
     )
 }
 
